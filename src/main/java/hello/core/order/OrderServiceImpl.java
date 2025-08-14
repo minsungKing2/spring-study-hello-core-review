966f1bf090667c2840032abbae74b38184f84ac9
MinSung Jung <minpro22@naver.com>
2025-08-14 13:54:11 +0900

    25.08.14 의존관계 자동 주입 관련 로직 및 테스크 코드 커밋

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,11 +6,13 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //주문 서비스 구현체(클라이언트)
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -21,11 +23,55 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    //의존관계 생성자 주입
+    //@Autowired 생략 가능 //생성자가 딱 1개만 있으면 @Autowired 를 생략해도 자동 주입 된다. 물론 스프링 빈에만 해당한다.
+    /*public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }*/
+
+    /*
+    //의존관계 수정자 주입(setter 주입)
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy
+    }
+
+    //의존관계 필드 주입(사용하지 않는 방식임.)
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private DiscountPolicy discountPolicy;
+
+    //의존관계 일반 메서드 주입(일반적으로 잘 사용하지 않음.)
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
+/*
+
+    //생성자 자동 주입
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+*/
+
+/*
+    //수정자 자동 주입
+    public DiscountPolicy setDiscountPolicy(@MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+
+*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
